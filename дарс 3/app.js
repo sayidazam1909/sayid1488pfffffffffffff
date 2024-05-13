280e8c386294d6073103aa4fc5005d33e76e172f
sayidazam1909 <163177180+sayidazam1909@users.noreply.github.com>
2024-05-13 10:14:53 +0500

    Add files via upload

---
+++ дарс 3/app.js
@@ -0,0 +1,19 @@
+
+
+
+
+let randomNumber = Math.floor(Math.random() * 100);
+
+let randomRequest = confirm('сейчас вам выйдет рандомное число');
+alert(`рандомное число ${randomNumber}`)
+let kopaytmaRequest = confirm('умножаем на 2')
+let bolinmaRequest = confirm('делим на 2')
+
+
+alert(`рандомное число ${randomNumber}
+
+умноженное на 2${randomNumber * 2}
+деленное на 2 ${randomNumber / 2}
+остаток когда делится на 3 ${randomNumber % 3}
+`)
+
